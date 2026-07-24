342d849cd05907c1314005d70850de6d8e318ec5
Shahaliyev <shahaliyev99@gmail.com>
2026-07-24 22:04:15 +0400

    Deployed f7b7e9e with MkDocs version: 1.6.1

--- assets/app/katex.js
+++ assets/app/katex.js
@@ -1,10 +1,41 @@
-document$.subscribe(({ body }) => { 
-    renderMathInElement(body, {
-      delimiters: [
-        { left: "$$",  right: "$$",  display: true },
-        { left: "$",   right: "$",   display: false },
-        { left: "\\(", right: "\\)", display: false },
-        { left: "\\[", right: "\\]", display: true }
-      ],
+(() => {
+  const KATEX_CSS_ATTR = "data-katex-css"
+
+  function katexCssHref() {
+    const scripts = document.getElementsByTagName("script")
+    for (const s of scripts) {
+      if (s.src && s.src.includes("/assets/app/katex.js")) {
+        return s.src.replace(/app\/katex\.js(?:\?.*)?$/, "vendor/katex/katex.min.css")
+      }
+    }
+    return "../../assets/vendor/katex/katex.min.css"
+  }
+
+  function ensureKatexCss() {
+    if (document.querySelector(`link[${KATEX_CSS_ATTR}]`)) {
+      return Promise.resolve()
+    }
+    return new Promise((resolve) => {
+      const link = document.createElement("link")
+      link.rel = "stylesheet"
+      link.href = katexCssHref()
+      link.setAttribute(KATEX_CSS_ATTR, "")
+      link.onload = () => resolve()
+      link.onerror = () => resolve()
+      document.head.appendChild(link)
+    })
+  }
+
+  document$.subscribe(({ body }) => {
+    ensureKatexCss().then(() => {
+      renderMathInElement(body, {
+        delimiters: [
+          { left: "$$",  right: "$$",  display: true },
+          { left: "$",   right: "$",   display: false },
+          { left: "\\(", right: "\\)", display: false },
+          { left: "\\[", right: "\\]", display: true }
+        ],
+      })
     })
   })
+})()
